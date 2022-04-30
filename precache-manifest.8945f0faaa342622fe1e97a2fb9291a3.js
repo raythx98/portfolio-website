e26f059bdd3cdcc7e7bc669ff88e4357f15a807e
Ray <raythx98@gmail.com>
2022-05-01 03:18:38 +0800

    Updates

---
+++ precache-manifest.8945f0faaa342622fe1e97a2fb9291a3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2078d89124b52c76cd67f8cec419c15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48f257975b48aa4f753c",
+    "url": "/static/css/main.fb882722.chunk.css"
+  },
+  {
+    "revision": "8ae1e97e20bf7321c005",
+    "url": "/static/js/2.56c71e8b.chunk.js"
+  },
+  {
+    "revision": "1a3cc71403f332d098a2b9cdd7c4c36d",
+    "url": "/static/js/2.56c71e8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48f257975b48aa4f753c",
+    "url": "/static/js/main.57543b00.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "3036b09b09bc19d402af7cd568331708",
+    "url": "/static/media/Ray Toh CV.3036b09b.pdf"
+  },
+  {
+    "revision": "adb7a25ab8333db81abf8165d443b16c",
+    "url": "/static/media/ray.adb7a25a.png"
+  }
+]);
